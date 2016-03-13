444465486055bb450c3defa3f5a1a6a56e883ef1
paine1690 <paine1690@126.com>
2016-03-13 21:10:02 +0800

    websocket
    先删了吧 暂时用不到

--- mysite/learn/views.py
+++ mysite/learn/views.py
@@ -4,8 +4,6 @@ from django.shortcuts import render
 #coding:utf-8
 from django.shortcuts import render
 import time
-from django_websocket import require_websocket
-from django_websocket import accept_websocket
 
 def GetNowTime():
     return time.strftime("%Y-%m-%d %H:%M:%S",time.localtime(time.time()))
