12753d9864a4cc3da1ddfc1a0cf62d026343f19d
paine1690 <paine1690@126.com>
2016-03-19 15:02:26 +0800

    weather

---
+++ mysite/learn/static/js/weather/config.js
@@ -0,0 +1,68 @@
+var config = {
+    lang: 'nl',
+    time: {
+        timeFormat: 12,
+        displaySeconds: true,
+        digitFade: false,
+    },
+    weather: {
+        //change weather params here:
+        //units: metric or imperial
+        params: {
+            q: 'Dalian,CN',
+            units: 'metric',
+            // if you want a different lang for the weather that what is set above, change it here
+            lang: 'en',
+            APPID: '4038dd9ebf114269a52984197cbdeb7a'
+        }
+    },
+    compliments: {
+        interval: 30000,
+        fadeInterval: 4000,
+        morning: [
+            'Good morning, handsome!',
+            'Enjoy your day!',
+            'How was your sleep?'
+        ],
+        afternoon: [
+            'Hello, beauty!',
+            'You look sexy!',
+            'Looking good today!'
+        ],
+        evening: [
+            'Wow, you look hot!',
+            'You look nice!',
+            'Hi, sexy!'
+        ]
+    },
+    calendar: {
+        maximumEntries: 10, // Total Maximum Entries
+		displaySymbol: true,
+		defaultSymbol: 'calendar', // Fontawsome Symbol see http://fontawesome.io/cheatsheet/
+        urls: [
+		{
+			symbol: 'calendar-plus-o', 
+			url: 'https://p01-calendarws.icloud.com/ca/subscribe/1/n6x7Farxpt7m9S8bHg1TGArSj7J6kanm_2KEoJPL5YIAk3y70FpRo4GyWwO-6QfHSY5mXtHcRGVxYZUf7U3HPDOTG5x0qYnno1Zr_VuKH2M'
+		},
+		{
+			symbol: 'soccer-ball-o',
+			url: 'https://www.google.com/calendar/ical/akvbisn5iha43idv0ktdalnor4%40group.calendar.google.com/public/basic.ics',
+		},
+		// {
+			// symbol: 'mars',
+			// url: "https://server/url/to/his.ics",
+		// },
+		// {
+			// symbol: 'venus',
+			// url: "https://server/url/to/hers.ics",
+		// },
+		// {
+			// symbol: 'venus-mars',
+			// url: "https://server/url/to/theirs.ics",
+		// },
+		]
+    },
+    news: {
+        feed: 'http://www.nytimes.com/services/xml/rss/nyt/HomePage.xml'
+    }
+}
---
+++ mysite/learn/static/js/weather/weather.js
@@ -0,0 +1,185 @@
+var weather = {
+	// Default language is Dutch because that is what the original author used
+	lang: config.lang || 'nl',
+	params: config.weather.params || null,
+	iconTable: {
+		'01d':'wi-day-sunny',
+		'02d':'wi-day-cloudy',
+		'03d':'wi-cloudy',
+		'04d':'wi-cloudy-windy',
+		'09d':'wi-showers',
+		'10d':'wi-rain',
+		'11d':'wi-thunderstorm',
+		'13d':'wi-snow',
+		'50d':'wi-fog',
+		'01n':'wi-night-clear',
+		'02n':'wi-night-cloudy',
+		'03n':'wi-night-cloudy',
+		'04n':'wi-night-cloudy',
+		'09n':'wi-night-showers',
+		'10n':'wi-night-rain',
+		'11n':'wi-night-thunderstorm',
+		'13n':'wi-night-snow',
+		'50n':'wi-night-alt-cloudy-windy'
+	},
+	temperatureLocation: '.temp',
+	windSunLocation: '.windsun',
+	forecastLocation: '.forecast',
+	apiVersion: '2.5',
+	apiBase: 'http://api.openweathermap.org/data/',
+	weatherEndpoint: 'weather',
+	forecastEndpoint: 'forecast/',
+	updateInterval: 6000,
+	fadeInterval: config.weather.fadeInterval || 1000,
+	intervalId: null,
+	orientation: config.weather.orientation || 'vertical',
+}
+
+/**
+ * Rounds a float to one decimal place
+ * @param  {float} temperature The temperature to be rounded
+ * @return {float}             The new floating point value
+ */
+weather.roundValue = function (temperature) {
+	return parseFloat(temperature).toFixed(1);
+}
+
+/**
+ * Converts the wind speed (km/h) into the values given by the Beaufort Wind Scale
+ * @see http://www.spc.noaa.gov/faq/tornado/beaufort.html
+ * @param  {int} kmh The wind speed in Kilometers Per Hour
+ * @return {int}     The wind speed converted into its corresponding Beaufort number
+ */
+weather.ms2Beaufort = function(ms) {
+	var kmh = ms * 60 * 60 / 1000;
+	var speeds = [1, 5, 11, 19, 28, 38, 49, 61, 74, 88, 102, 117, 1000];
+	for (var beaufort in speeds) {
+		var speed = speeds[beaufort];
+		if (speed > kmh) {
+			return beaufort;
+		}
+	}
+	return 12;
+}
+
+/**
+ * Retrieves the current temperature and weather patter from the OpenWeatherMap API
+ */
+weather.updateCurrentWeather = function () {
+
+	$.ajax({
+		type: 'GET',
+		url: weather.apiBase + '/' + weather.apiVersion + '/' + weather.weatherEndpoint,
+		dataType: 'json',
+		data: weather.params,
+		success: function (data) {
+			var _temperature = this.roundValue(data.main.temp),
+				_temperatureMin = this.roundValue(data.main.temp_min),
+				_temperatureMax = this.roundValue(data.main.temp_max),
+				_wind = this.roundValue(data.wind.speed),
+				_iconClass = this.iconTable[data.weather[0].icon];
+
+			var _icon = '<span class="icon ' + _iconClass + ' dimmed wi"></span>';
+
+			var _newTempHtml = _icon + '' + _temperature + '&deg;';
+
+			$(this.temperatureLocation).updateWithText(_newTempHtml, this.fadeInterval);
+
+			var _now = moment().format('HH:mm'),
+				_sunrise = moment(data.sys.sunrise*1000).format('HH:mm'),
+				_sunset = moment(data.sys.sunset*1000).format('HH:mm');
+
+			var _newWindHtml = '<span class="wind"><span class="wi wi-strong-wind xdimmed"></span> ' + this.ms2Beaufort(_wind) + '</span>',
+				_newSunHtml = '<span class="sun"><span class="wi wi-sunrise xdimmed"></span> ' + _sunrise + '</span>';
+
+			if (_sunrise < _now && _sunset > _now) {
+				_newSunHtml = '<span class="sun"><span class="wi wi-sunset xdimmed"></span> ' + _sunset + '</span>';
+			}
+
+			$(this.windSunLocation).updateWithText(_newWindHtml + ' ' + _newSunHtml,this.fadeInterval);
+
+		}.bind(this),
+		error: function () {
+			alert("weather error");
+		}
+	});
+
+}
+
+/**
+ * Updates the 5 Day Forecast from the OpenWeatherMap API
+ */
+weather.updateWeatherForecast = function () {
+
+	$.ajax({
+		type: 'GET',
+		url: weather.apiBase + '/' + weather.apiVersion + '/' + weather.forecastEndpoint,
+		data: weather.params,
+		success: function (data) {
+
+			var _opacity = 1,
+				_forecastHtml = '<tr>',
+				_forecastHtml2 = '<tr>',
+				_forecastHtml3 = '<tr>',
+				_forecastHtml4 = '<tr>';
+
+			_forecastHtml = '<table class="forecast-table"><tr>';
+
+			for (var i = 0, count = data.list.length; i < count; i++) {
+
+				var _forecast = data.list[i];
+				
+				if (this.orientation == 'vertical') {
+					_forecastHtml2 = '';
+					_forecastHtml3 = '';
+					_forecastHtml4 = '';
+				}
+
+				_forecastHtml += '<td style="opacity:' + _opacity + '" class="day">' + moment(_forecast.dt, 'X').format('ddd') + '</td>';
+				_forecastHtml2 += '<td style="opacity:' + _opacity + '" class="icon-small ' + this.iconTable[_forecast.weather[0].icon] + '"></td>';
+				
+
+				_opacity -= 0.155;
+
+				if (this.orientation == 'vertical') {
+					_forecastHtml += _forecastHtml2 + _forecastHtml3 + _forecastHtml4 + '</tr>';
+				}
+			}
+			_forecastHtml  += '</tr>',
+			_forecastHtml2 += '</tr>',
+			_forecastHtml3 += '</tr>',
+			_forecastHtml4 += '</tr>';
+			
+			if (this.orientation == 'vertical') {
+				_forecastHtml += '</table>';
+			} else {
+				_forecastHtml += _forecastHtml2 + _forecastHtml3 + _forecastHtml4 +'</table>';
+			}
+
+			$(this.forecastLocation).updateWithText(_forecastHtml, this.fadeInterval);
+
+		}.bind(this),
+		error: function () {
+			alert("error");
+		}
+	});
+
+}
+
+weather.init = function () {
+
+	if (this.params.lang === undefined) {
+		this.params.lang = this.lang;
+	}
+
+	if (this.params.cnt === undefined) {
+		this.params.cnt = 6;
+	}
+
+	this.intervalId = setInterval(function () {
+		this.updateCurrentWeather();
+		
+	}.bind(this), this.updateInterval);
+	this.updateCurrentWeather();
+	
+}
