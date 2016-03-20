42889621f1e03390dcf403b2f4483026232bc698
paine1690 <paine1690@126.com>
2016-03-20 09:35:22 +0800

    pictrue

---
+++ mysite/learn/static/js/pictrue/theme.js
@@ -0,0 +1,16 @@
+/* ------------------------------------------------------------------------ */
+/* Background Slider
+/* ------------------------------------------------------------------------ */
+
+$('body').vegas({	
+	slides: [
+		{ src: 'http://localhost:8000/static/img/pic1.jpg' },
+		{ src: 'http://localhost:8000/static/img/pic2.jpg' },
+		{ src: 'http://localhost:8000/static/img/pic3.jpg' },
+		{ src: 'http://localhost:8000/static/img/pic4.jpg' },
+		{ src: 'http://localhost:8000/static/img/pic5.jpg' },
+		{ src: 'http://localhost:8000/static/img/pic6.jpg' },
+		{ src: 'http://localhost:8000/static/img/pic7.jpg' },
+	],
+});
+
